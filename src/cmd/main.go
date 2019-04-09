bea412258707cd2780e42487aac2ab2e457e28a8
jonas27 <jonas.burster@gmail.com>
2019-04-09 11:52:18 +0200

    v1:release

---
+++ src/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", homeHandler)
+	http.HandleFunc("/hello/hello", helloHandler)
+	http.HandleFunc("/hello/info", infoHandler)
+	http.ListenAndServe(":3000", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		fmt.Fprint(w, "This is your path\n")
+		fmt.Fprintln(w, r.URL.Path)
+		return
+	}
+	fmt.Fprint(w, "welcome home\n")
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "V1: welcome to hello\n")
+}
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "V1\n")
+}
